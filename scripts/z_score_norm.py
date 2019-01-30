393b6575f6dc41ac5a1db5769a2e98d6936a85b9
Jay Wong <xiao.hk1997@gmail.com>
2019-01-30 09:19:42 -0600

    Add other normalization ideas

--- scripts/z_score_norm.py
+++ scripts/z_score_norm.py
@@ -79,6 +79,41 @@ def compute_stats(feature_dict, dmso_feature_dict, names):
              dmso_sd_dict=dmso_sd_dict, problem_sd_dict=problem_sd_dict)
 
 
+def compute_stats_all_feature(feature_dict, names):
+    """
+    Use the mean and sd from all features within that plate to normalize.
+    If the sd is 0 (unlikely), then we give up normalizing that feature.
+    """
+    # Then we want to aggregate the features within each plate
+    pids = set()
+    for name in names:
+        pids.add(int(re.sub(r'(\d{5})_.+', r'\1', name)))
+
+    mean_dict = {}
+    sd_dict = {}
+    problem_sd_dict = {}
+
+    for p in pids:
+        # Use all feature's mean and std
+        mean_dict[p] = np.mean(feature_dict[p], axis=0)
+        sd_dict[p] = np.std(feature_dict[p], axis=0)
+
+        # If the sd of all images is still 0, then we just don't
+        # normalize this feature and add it to a special list
+        for col in np.where(sd_dict[p] == 0)[0]:
+            print("Plate {}'s {} feature has 0 std.".format(p, col))
+            mean_dict[p][col] = 0
+            sd_dict[p][col] = 1
+            if p in problem_sd_dict:
+                problem_sd_dict[p].append(col)
+            else:
+                problem_sd_dict[p] = [col]
+
+    # Save the results
+    np.savez("z_score_norm_dicts_all.npz", mean_dict=mean_dict,
+             sd_dict=sd_dict, problem_sd_dict=problem_sd_dict)
+
+
 def z_score_norm(feature, name, mean_dict, sd_dict):
     """
     Use z-score to normalize the given feature.
@@ -126,7 +161,7 @@ def vis_batch_effect(dmso_feature_dict, out_name="correlation.png"):
     fig.savefig(out_name, bbox_inches="tight")
 
 
-def main_norm():
+def main_norm(dmso_feature=True):
     """
     The main function to normalize the features.
     """
@@ -138,12 +173,20 @@ def main_norm():
 
     # Generate the mean/std statistics first
     feature_dict, dmso_feature_dict = group_features(features, names)
-    compute_stats(feature_dict, dmso_feature_dict, names)
+    if dmso_feature:
+        compute_stats(feature_dict, dmso_feature_dict, names)
+    else:
+        compute_stats_all_feature(feature_dict, names)
 
     # Get the mean and sd dictionaries
-    dicts = np.load("./z_score_norm_dicts.npz")
-    dmso_mean_dict = dicts["dmso_mean_dict"].item()
-    dmso_sd_dict = dicts["dmso_sd_dict"].item()
+    if dmso_feature:
+        dicts = np.load("./z_score_norm_dicts.npz")
+        mean_dict = dicts["dmso_mean_dict"].item()
+        sd_dict = dicts["dmso_sd_dict"].item()
+    else:
+        dicts = np.load("./z_score_norm_dicts_all.npz")
+        mean_dict = dicts["mean_dict"].item()
+        sd_dict = dicts["sd_dict"].item()
 
     normed_features = []
 
@@ -152,16 +195,20 @@ def main_norm():
         cur_name = names[i]
         normed_features.append(z_score_norm(cur_feature,
                                             cur_name,
-                                            dmso_mean_dict,
-                                            dmso_sd_dict))
+                                            mean_dict,
+                                            sd_dict))
 
     features = None
     normed_features = np.vstack(normed_features)
-    np.savez("normed_features.npz", features=normed_features, names=names,
+    if dmso_feature:
+        normed_name = "normed_features.npz"
+    else:
+        normed_name = "normed_features_group.npz"
+    np.savez(normed_name, features=normed_features, names=names,
              sids=sids)
 
 
-def main_vis():
+def main_vis(normed_name, output_name):
     """
     The main function to visualize batch effects.
     """
@@ -175,22 +222,64 @@ def main_vis():
     # Plot the correlation matrix
     feature_dict, dmso_feature_dict = group_features(features, names)
     vis_batch_effect(dmso_feature_dict, out_name="before_correlation.png")
-
-    # Load the features and their names after normalization
     """
 
-    data = np.load("./resource/normed_features.npz")
+    # Load the features and their names after normalization
+    data = np.load(normed_name)
     features = data["features"]
     names = data["names"]
 
     # Plot the correlation matrix
     feature_dict, dmso_feature_dict = group_features(features, names)
-    vis_batch_effect(dmso_feature_dict, out_name="after_correlation.png")
+    vis_batch_effect(dmso_feature_dict, out_name=output_name)
 
 
-main_vis()
+def main_norm_all_dmso():
+    """
+    Compute the mean and sd of all DMSO images (not grouped by plates).
+    """
+    # Load the features and their names
+    data = np.load("./resource/combined_feature_406.npz")
+    features = data["features"]
+    names = data["names"]
+    sids = data["sids"]
+
+    # Get the mean and sd from all DMSO images
+    dmso_index = [True if "DMSO" in d else False for d in names]
+
+    dmso_features = features[dmso_index]
+    dmso_mean = np.mean(dmso_features, axis=0)
+    dmso_sd = np.std(dmso_features, axis=0)
+
+    # Check if there are 0 sd feature (should be very very few)
+    for col in np.where(dmso_sd == 0)[0]:
+        print("Found zero std at col = {}".format(col))
+        if np.std(features[:, col]) != 0:
+            # Use the mean and std from all images
+            dmso_mean[col] = np.mean(features[:, col])
+            dmso_sd[col] = np.std(features[:, col])
+        else:
+            # Give up normalizing this feature
+            print("Give up normalizing col = {}".format(col))
+            dmso_mean[col] = 0
+            dmso_sd[col] = 1
+
+    normed_features = []
+
+    for i in range(features.shape[0]):
+        cur_feature = features[i, :]
+        normed_features.append(
+            np.divide((cur_feature - dmso_mean), dmso_sd)
+        )
+
+    features = None
+
+    normed_features = np.vstack(normed_features)
+    np.savez("./resource/normed_features_all.npz",
+             features=normed_features, names=names,
+             sids=sids)
 
 
-
-
+main_norm(dmso_feature=False)
+main_vis("normed_feature_group.npz", "after_normed_group.png")
 
